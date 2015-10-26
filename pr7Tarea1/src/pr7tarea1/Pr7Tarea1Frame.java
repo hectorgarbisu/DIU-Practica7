eaae00b0457c50a3976e60f76d798b1f67a94179
hectorgarbisu <geco.hh@gmail.com>
2015-10-26 22:54:50 +0000

    Plop

--- pr7Tarea1/src/pr7tarea1/Pr7Tarea1Frame.java
+++ pr7Tarea1/src/pr7tarea1/Pr7Tarea1Frame.java
@@ -9,12 +9,12 @@ package pr7tarea1;
  *
  * @author Héctor Garbisu
  */
-public class MainFrame extends javax.swing.JFrame {
+public class Pr7Tarea1Frame extends javax.swing.JFrame {
 
     /**
      * Creates new form MainFrame
      */
-    public MainFrame() {
+    public Pr7Tarea1Frame() {
         initComponents();
     }
 
@@ -60,20 +60,21 @@ public class MainFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Pr7Tarea1Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Pr7Tarea1Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Pr7Tarea1Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Pr7Tarea1Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MainFrame().setVisible(true);
+                new Pr7Tarea1Frame().setVisible(true);
             }
         });
     }
